676b13eea08af267d806710a4e0750b2564ffff3
Edwin Aguilar <edwin.aguilar@unmsm.edu.pe>
2022-05-05 11:44:22 -0500

    Initial configuration

---
+++ src/main/java/com/example/demo/entity/Product.java
@@ -0,0 +1,7 @@
+package com.example.demo.entity;
+
+public class Product {
+	private String name;
+	private Double price;
+	private Integer stock;
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO products (name, price, stock) VALUES 
+(Oreo, 1, 100),
+(Pepsi, 3, 100),
+(Hershey, 2, 100)
